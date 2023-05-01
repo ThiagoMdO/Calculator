ac65aa9728f1cc67092ec3d054355f4290bf6387
ThiagoMdO <128644651+ThiagoMdO@users.noreply.github.com>
2023-05-01 11:17:15 -0300

    Acrescentado mais interação no Display e corrido logicas para não fazer operações de 'NaN'

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-var firstNum
-var secondNum
+var firstNum =null;
+var secondNum =null;
 var operation
 var number
-var fullNumber = ''
+var fullNumber = '';
 const display = document.getElementById('displayCalc')
-var answer;
+var answer='';
 var signal;
 var sizeString = fullNumber.length;
 
+/*
+variável operador aritmético, para exibir no display - talvez a propria variavel
+signal possa fazer a mesma função da var operador;
+*/
+var operador='';
+
+
 // display functions
-function displayWrite(escrever) {   
+function displayWrite(escrever) {
     display.innerHTML = escrever;
 }
 function displayClear() {
     display.innerHTML = "";
+    answer='';
     reset(0);
 }
 function reset(clear) {
@@ -21,34 +29,88 @@ function reset(clear) {
     if(clear == 1){displayClear();}
 }
 function operation(inputSignal) {
+    
     signal = inputSignal;
+    switch(signal){
+        case 'som':
+            operador=' + ';
+            break;
+        case 'sub':
+            operador=' - ';
+            break;
+        case 'mult':
+            operador=' * ';
+            break;
+        case 'div':
+            operador=' / ';
+            break;
+        default:
+            operador='NaN';
+    }
+    if(answer!=''){
+        firstNum = answer;
+    }else{
     saveFirstNumber();
+    }
+    displayWrite(firstNum+' '+operador);
 }
 function saveFirstNumber(){
     firstNum = parseFloat(fullNumber);   
-    reset(1);
+    answer='';
+    fullNumber = "";
 }
 
 
 // math functions
 function breakApart(number) {
-    fullNumber = fullNumber + number
-    displayWrite(fullNumber)
+    fullNumber = fullNumber + number;
+    displayWrite(fullNumber);
+    
 }
 
 function result() {
-    answer = parseInt(answer);
-    displayWrite(answer);
+    answer = parseFloat(answer);
+    if(firstNum==null||isNaN(secondNum)||isNaN(answer)){
+        displayWrite('Operação invalida');
+        secondNum == '';
+        answer == '';
+    }else{
+        displayWrite(firstNum+operador+secondNum+' = '+answer);
+    }
+    
 }
+
 // Calculate
 function calculate() {
     secondNum = parseFloat(fullNumber);
-
-    if(signal == 'som'){answer = firstNum + secondNum;;}
-    else if(signal == 'sub'){answer = firstNum - secondNum;}
-    else if(signal == 'mult'){answer = firstNum * secondNum;}
-    else if(signal == 'div'){answer = firstNum / secondNum;}
-    result();
+        switch(signal){
+            case 'som':
+                operador = ' + ';
+                answer = firstNum + secondNum;
+                break;
+            case 'sub':
+                operador = ' - ';
+                answer = firstNum - secondNum;            
+                break;
+            case 'mult':
+                operador = ' * ';
+                answer = firstNum * secondNum;            
+                break;
+            case 'div':
+                operador = ' / ';
+                answer = firstNum / secondNum;            
+                break;
+            default:
+                answer = '';
+        }
+        /*
+        if(signal == 'som'){answer = firstNum + secondNum;;}
+        else if(signal == 'sub'){answer = firstNum - secondNum;}
+        else if(signal == 'mult'){answer = firstNum * secondNum;}
+        else if(signal == 'div'){answer = firstNum / secondNum;}
+        */
+        result();
+        fullNumber = '';    
 }
 
 function del() {
